23de75db220192a1f9629904614094dc5769e8fe
bahadirgltkn <63556574+bahadirgltkn@users.noreply.github.com>
2021-07-27 18:23:59 +0300

    SQL task 12

---
+++ SQL Task/SQL Task 12/task12.sql
@@ -0,0 +1,33 @@
+-- 1 --
+SELECT COUNT(*) FROM film 
+WHERE length > 
+(
+	SELECT AVG(length) FROM film
+);
+
+-- 2 --
+SELECT COUNT(*) FROM film 9
+WHERE rental_rate = 
+(
+	SELECT MAX(rental_rate) FROM film
+)
+
+-- 3 -- 
+SELECT * FROM film
+WHERE rental_rate = 
+(
+	SELECT MIN(rental_rate) FROM film
+)
+AND replacement_cost = 
+(
+	SELECT MIN(replacement_cost) FROM film
+);
+
+-- 4 -- 
+SELECT * FROM Customer
+JOIN payment
+ON payment.customer_id = customer.customer_id
+WHERE amount = 
+(
+	SELECT MAX(amount) FROM payment
+);
